0dcbc9b8af6906761e68b543fecbf465874b29fc
Nhat Hung <doraemonidol@gmail.com>
2023-08-30 10:23:38 +0700

    firebase connect & style sheet

---
+++ lib/helpers/style.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+class FigmaColors {
+  const FigmaColors();
+
+  static const Color lightGrey = Color(0xfff1f1f1);
+  static const Color lightblue = Color(0xffc4d7ff);
+  static const Color sUNRISEBluePrimary = Color(0xff2fd1c5);
+  static const Color sUNRISELightCoral = Color(0xffe4edff);
+  static const Color sUNRISESunray = Color(0xfff5fbff);
+  static const Color sUNRISELightCharcoal = Color(0xff00394c);
+  static const Color sUNRISECharcoal = Color(0xff37313c);
+  static const Color sUNRISEDarkCharcoal = Color(0xff2f2a32);
+  static const Color sUNRISETextGrey = Color(0xff585a66);
+  static const Color sUNRISEDarkGrey = Color(0xffcccccc);
+  static const Color sUNRISEGrey = Color(0xffffffff);
+  static const Color sUNRISEWhite = Color(0xffffffff);
+  static const Color sUNRISEErrorRed = Color(0xffdc7603);
+  static const Color sUNRISEWaves = Color(0xffe48fff);
+  static const Color systemGrey = Color(0xe59a9a9a);
+}
+
+class FigmaTextStyles {
+  const FigmaTextStyles();
+
+  TextStyle get h1 => const TextStyle(
+        fontSize: 55,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-ExtraBold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 66 / 55,
+        letterSpacing: 0,
+      );
+
+  TextStyle get h2 => const TextStyle(
+        fontSize: 40,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Bold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 48 / 40,
+        letterSpacing: 0,
+      );
+
+  TextStyle get h3 => const TextStyle(
+        fontSize: 30,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Bold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 36 / 30,
+        letterSpacing: 0,
+      );
+
+  TextStyle get h4 => const TextStyle(
+        fontSize: 21,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Bold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 25.2 / 21,
+        letterSpacing: 0,
+      );
+
+  TextStyle get b => const TextStyle(
+        fontSize: 18,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 21.6 / 18,
+        letterSpacing: 0,
+      );
+
+  TextStyle get p => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 22.39 / 16,
+        letterSpacing: 0,
+      );
+
+  TextStyle get t => const TextStyle(
+        fontSize: 12,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 16.79 / 12,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mH1 => const TextStyle(
+        fontSize: 36,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-ExtraBold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 43.2 / 36,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mH2 => const TextStyle(
+        fontSize: 26,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Bold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 31.2 / 26,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mH3 => const TextStyle(
+        fontSize: 20,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-SemiBold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 24 / 20,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mH4 => const TextStyle(
+        fontSize: 18,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 21.6 / 18,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mB => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 19.2 / 16,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mP => const TextStyle(
+        fontSize: 14,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 19.59 / 14,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mT => const TextStyle(
+        fontSize: 12,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Regular',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 16.79 / 12,
+        letterSpacing: 0,
+      );
+
+  TextStyle get buttons => const TextStyle(
+        fontSize: 21,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Bold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 25.2 / 21,
+        letterSpacing: 0,
+      );
+
+  TextStyle get mButton => const TextStyle(
+        fontSize: 18,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-SemiBold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 21.6 / 18,
+        letterSpacing: 0,
+      );
+
+  TextStyle get sButton => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontFamily: 'Poppins-Bold',
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 19.2 / 16,
+        letterSpacing: 0,
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:codev/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fa;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await fa.FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -129,7 +140,6 @@ class MyApp extends StatelessWidget {
 // If you do not have a themeMode switch, uncomment this line
 // to let the device system mode control the theme mode:
       themeMode: ThemeMode.system,
-
       home: const MyHomePage(title: 'CoDev'),
     );
   }
