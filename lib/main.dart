b3f0e2535f1eff628a93c9e3d3fc35ed16d9a0f2
Nhat Hung <doraemonidol@gmail.com>
2023-08-31 22:25:00 +0700

    baseline for login
    using provider for auth
    + auth: all the authentication methods
    + user: người dùng có attribute gì thì này nó quản lý
    + auth_screen: có auth_card là 1 cái card có thể chuyển từ login sang sign up hoặc ngược lại

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ import './screens/notification_screen.dart';
 import './screens/profile_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/tasks_screen.dart';
+import 'providers/auth.dart';
+import 'screens/auth_screen.dart';
+import 'screens/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,11 +31,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return /* MultiProvider(
+    return MultiProvider(
       providers: [
-        // ChangeNotifierProvider(
-        //   create: (ctx) => Auth(),
-        // ),
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         // ChangeNotifierProxyProvider<Auth, Products>(
         //   update: (ctx, auth, previousProduct) => Products(
         //     auth.token,
@@ -42,117 +45,131 @@ class MyApp extends StatelessWidget {
         //   create: (ctx) => Products('', '', []),
         // ),
       ],
-      child:  Consumer<Auth>(
-        builder: (context, auth, _) => */
-        MaterialApp(
-      title: 'CoDev',
-      theme: FlexThemeData.light(
-        scheme: FlexScheme.bahamaBlue,
-        usedColors: 2,
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 4,
-        appBarStyle: FlexAppBarStyle.background,
-        bottomAppBarElevation: 1.0,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 10,
-          blendOnColors: false,
-          blendTextTheme: true,
-          useTextTheme: true,
-          useM2StyleDividerInM3: true,
-          thickBorderWidth: 2.0,
-          elevatedButtonSchemeColor: SchemeColor.onPrimaryContainer,
-          elevatedButtonSecondarySchemeColor: SchemeColor.primaryContainer,
-          inputDecoratorSchemeColor: SchemeColor.primary,
-          inputDecoratorBackgroundAlpha: 12,
-          inputDecoratorRadius: 8.0,
-          inputDecoratorUnfocusedHasBorder: false,
-          inputDecoratorPrefixIconSchemeColor: SchemeColor.primary,
-          appBarScrolledUnderElevation: 8.0,
-          drawerElevation: 1.0,
-          drawerWidth: 290.0,
-          bottomNavigationBarSelectedLabelSchemeColor: SchemeColor.secondary,
-          bottomNavigationBarMutedUnselectedLabel: false,
-          bottomNavigationBarSelectedIconSchemeColor: SchemeColor.secondary,
-          bottomNavigationBarMutedUnselectedIcon: false,
-          navigationBarSelectedLabelSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationBarSelectedIconSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
-          navigationBarIndicatorOpacity: 1.00,
-          navigationBarElevation: 1.0,
-          navigationBarHeight: 72.0,
-          navigationRailSelectedLabelSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationRailSelectedIconSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
-          navigationRailIndicatorOpacity: 1.00,
-        ),
-        useMaterial3ErrorColors: true,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        // To use the Playground font, add GoogleFonts package and uncomment
-        // fontFamily: GoogleFonts.notoSans().fontFamily,
-      ),
-      darkTheme: FlexThemeData.dark(
-        colors: FlexColor.schemes[FlexScheme.bahamaBlue]!.light.defaultError
-            .toDark(40, false),
-        usedColors: 2,
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 10,
-        appBarStyle: FlexAppBarStyle.background,
-        bottomAppBarElevation: 2.0,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 20,
-          blendTextTheme: true,
-          useTextTheme: true,
-          useM2StyleDividerInM3: true,
-          thickBorderWidth: 2.0,
-          elevatedButtonSchemeColor: SchemeColor.onPrimaryContainer,
-          elevatedButtonSecondarySchemeColor: SchemeColor.primaryContainer,
-          inputDecoratorSchemeColor: SchemeColor.primary,
-          inputDecoratorBackgroundAlpha: 48,
-          inputDecoratorRadius: 8.0,
-          inputDecoratorUnfocusedHasBorder: false,
-          inputDecoratorPrefixIconSchemeColor: SchemeColor.primary,
-          drawerElevation: 1.0,
-          drawerWidth: 290.0,
-          bottomNavigationBarSelectedLabelSchemeColor: SchemeColor.secondary,
-          bottomNavigationBarMutedUnselectedLabel: false,
-          bottomNavigationBarSelectedIconSchemeColor: SchemeColor.secondary,
-          bottomNavigationBarMutedUnselectedIcon: false,
-          navigationBarSelectedLabelSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationBarSelectedIconSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
-          navigationBarIndicatorOpacity: 1.00,
-          navigationBarElevation: 1.0,
-          navigationBarHeight: 72.0,
-          navigationRailSelectedLabelSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationRailSelectedIconSchemeColor:
-              SchemeColor.onSecondaryContainer,
-          navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
-          navigationRailIndicatorOpacity: 1.00,
-        ),
-        useMaterial3ErrorColors: true,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        // To use the Playground font, add GoogleFonts package and uncomment
-        // fontFamily: GoogleFonts.notoSans().fontFamily,
-      ),
+      child: Consumer<Auth>(
+        builder: (context, auth, _) => MaterialApp(
+          title: 'CoDev',
+          theme: FlexThemeData.light(
+            scheme: FlexScheme.bahamaBlue,
+            usedColors: 2,
+            surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+            blendLevel: 4,
+            appBarStyle: FlexAppBarStyle.background,
+            bottomAppBarElevation: 1.0,
+            subThemesData: const FlexSubThemesData(
+              blendOnLevel: 10,
+              blendOnColors: false,
+              blendTextTheme: true,
+              useTextTheme: true,
+              useM2StyleDividerInM3: true,
+              thickBorderWidth: 2.0,
+              elevatedButtonSchemeColor: SchemeColor.onPrimaryContainer,
+              elevatedButtonSecondarySchemeColor: SchemeColor.primaryContainer,
+              inputDecoratorSchemeColor: SchemeColor.primary,
+              inputDecoratorBackgroundAlpha: 12,
+              inputDecoratorRadius: 8.0,
+              inputDecoratorUnfocusedHasBorder: false,
+              inputDecoratorPrefixIconSchemeColor: SchemeColor.primary,
+              appBarScrolledUnderElevation: 8.0,
+              drawerElevation: 1.0,
+              drawerWidth: 290.0,
+              bottomNavigationBarSelectedLabelSchemeColor:
+                  SchemeColor.secondary,
+              bottomNavigationBarMutedUnselectedLabel: false,
+              bottomNavigationBarSelectedIconSchemeColor: SchemeColor.secondary,
+              bottomNavigationBarMutedUnselectedIcon: false,
+              navigationBarSelectedLabelSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationBarSelectedIconSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+              navigationBarIndicatorOpacity: 1.00,
+              navigationBarElevation: 1.0,
+              navigationBarHeight: 72.0,
+              navigationRailSelectedLabelSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationRailSelectedIconSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationRailIndicatorSchemeColor:
+                  SchemeColor.secondaryContainer,
+              navigationRailIndicatorOpacity: 1.00,
+            ),
+            useMaterial3ErrorColors: true,
+            visualDensity: FlexColorScheme.comfortablePlatformDensity,
+            useMaterial3: true,
+            // To use the Playground font, add GoogleFonts package and uncomment
+            // fontFamily: GoogleFonts.notoSans().fontFamily,
+          ),
+          darkTheme: FlexThemeData.dark(
+            colors: FlexColor.schemes[FlexScheme.bahamaBlue]!.light.defaultError
+                .toDark(40, false),
+            usedColors: 2,
+            surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+            blendLevel: 10,
+            appBarStyle: FlexAppBarStyle.background,
+            bottomAppBarElevation: 2.0,
+            subThemesData: const FlexSubThemesData(
+              blendOnLevel: 20,
+              blendTextTheme: true,
+              useTextTheme: true,
+              useM2StyleDividerInM3: true,
+              thickBorderWidth: 2.0,
+              elevatedButtonSchemeColor: SchemeColor.onPrimaryContainer,
+              elevatedButtonSecondarySchemeColor: SchemeColor.primaryContainer,
+              inputDecoratorSchemeColor: SchemeColor.primary,
+              inputDecoratorBackgroundAlpha: 48,
+              inputDecoratorRadius: 8.0,
+              inputDecoratorUnfocusedHasBorder: false,
+              inputDecoratorPrefixIconSchemeColor: SchemeColor.primary,
+              drawerElevation: 1.0,
+              drawerWidth: 290.0,
+              bottomNavigationBarSelectedLabelSchemeColor:
+                  SchemeColor.secondary,
+              bottomNavigationBarMutedUnselectedLabel: false,
+              bottomNavigationBarSelectedIconSchemeColor: SchemeColor.secondary,
+              bottomNavigationBarMutedUnselectedIcon: false,
+              navigationBarSelectedLabelSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationBarSelectedIconSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+              navigationBarIndicatorOpacity: 1.00,
+              navigationBarElevation: 1.0,
+              navigationBarHeight: 72.0,
+              navigationRailSelectedLabelSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationRailSelectedIconSchemeColor:
+                  SchemeColor.onSecondaryContainer,
+              navigationRailIndicatorSchemeColor:
+                  SchemeColor.secondaryContainer,
+              navigationRailIndicatorOpacity: 1.00,
+            ),
+            useMaterial3ErrorColors: true,
+            visualDensity: FlexColorScheme.comfortablePlatformDensity,
+            useMaterial3: true,
+            // To use the Playground font, add GoogleFonts package and uncomment
+            // fontFamily: GoogleFonts.notoSans().fontFamily,
+          ),
 // If you do not have a themeMode switch, uncomment this line
 // to let the device system mode control the theme mode:
-      themeMode: ThemeMode.system,
-      home: TabsScreen(),
-      routes: {
-        AgendaScreen.routeName: (ctx) => AgendaScreen(),
-        NotificationScreen.routeName: (ctx) => NotificationScreen(),
-        ProfileScreen.routeName: (ctx) => ProfileScreen(),
-        TasksScreen.routeName: (ctx) => TasksScreen(),
-      },
+          themeMode: ThemeMode.system,
+          home: auth.isAuth
+              ? TabsScreen()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(context),
+                  builder: (cxt, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
+          routes: {
+            AgendaScreen.routeName: (ctx) => AgendaScreen(),
+            NotificationScreen.routeName: (ctx) => NotificationScreen(),
+            ProfileScreen.routeName: (ctx) => ProfileScreen(),
+            TasksScreen.routeName: (ctx) => TasksScreen(),
+          },
+        ),
+      ),
     );
   }
 }
--- lib/providers/user.dart
+++ lib/providers/user.dart
@@ -1,1 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'dart:convert';
 
+class User with ChangeNotifier {
+  String email;
+  String phoneNumber;
+  String name;
+  String address;
+  String imageUrl;
+  String authToken;
+  String userId;
+
+  User({
+    this.authToken = '',
+    this.userId = '',
+    this.email = 'Unknown',
+    this.phoneNumber = 'Unknown',
+    this.name = 'You Guys',
+    this.address = 'Unknown',
+    this.imageUrl =
+        'https://upload.wikimedia.org/wikipedia/vi/b/b1/1989Deluxe.jpeg',
+  }) {
+    if (authToken == '') return;
+    fetchAndSetUser();
+    //print(userId);
+  }
+
+  get token {
+    return authToken;
+  }
+
+  Future<void> fetchAndSetUser([context]) async {
+    print('fetching user');
+    final url =
+        'https://coffee-shop-1989-default-rtdb.asia-southeast1.firebasedatabase.app/users/$userId.json?auth=$authToken';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      final extractedData = json.decode(response.body);
+      print(response.body);
+      if (extractedData == null) {
+        return;
+      }
+
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> addUser(String email) async {
+    print('adding user');
+    final url =
+        'https://coffee-shop-1989-default-rtdb.asia-southeast1.firebasedatabase.app/users/$userId.json?auth=$authToken';
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode({}),
+      );
+
+      print(response.body);
+
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+
+  Future<void> updateUser({
+    String? newEmail,
+    String? newPhoneNumber,
+    String? newName,
+    String? newAddress,
+    String? newImageUrl,
+  }) async {
+    final url =
+        'https://coffee-shop-1989-default-rtdb.asia-southeast1.firebasedatabase.app/users/$userId.json?auth=$authToken';
+
+    newEmail = newEmail == null ? email : newEmail;
+    newPhoneNumber = newPhoneNumber == null ? phoneNumber : newPhoneNumber;
+    newName = newName == null ? name : newName;
+    newAddress = newAddress == null ? address : newAddress;
+    newImageUrl = newImageUrl == null ? imageUrl : newImageUrl;
+
+    await http.patch(
+      Uri.parse(url),
+      body: json.encode({
+        'email': newEmail,
+        'phoneNumber': newPhoneNumber,
+        'name': newName,
+        'address': newAddress,
+        'imageUrl': newImageUrl,
+      }),
+    );
+    email = newEmail;
+    phoneNumber = newPhoneNumber;
+    name = newName;
+    address = newAddress;
+    imageUrl = newImageUrl;
+    notifyListeners();
+  }
+}
