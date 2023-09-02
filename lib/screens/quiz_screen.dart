4fe70e84479168a230e14f6c3063f9f5c9c92024
HOANG THE ANH <therealhoangtheanh@gmail.com>
2023-09-02 12:18:03 +0700

    Revert "Sign Up Quiz initialization"
    This reverts commit 22a400fe9a9fe9a7882c43a7a37ff6c35b0f4d92.

---
+++ lib/screens/quiz_screen.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _QuizScreen();
+  
+}
+
+class _QuizScreen extends State<QuizScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
