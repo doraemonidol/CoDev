5be2558689ee5d017433f2307eeafcd627994359
Nhat Hung <doraemonidol@gmail.com>
2023-09-04 08:27:48 +0700

    content and responsive main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,8 +4,16 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 //test for sliders
-final titles = ['Study Planner', 'Build Your Future', 'Early Bird'];
-final subtitles = ['abcd', 'efgh', 'ijkl'];
+final titles = [
+  'Welcome to CoDev',
+  'Choose Your Path',
+  'Personalized Learning'
+];
+final subtitles = [
+  'Start your journey into the exciting world of Information Technology with CoDev. Explore various fields, gain valuable skills, and prepare for a successful IT career.',
+  'Discover the diverse fields within IT, from cybersecurity to web development. Customize your learning experience by selecting the field that interests you the most. Let\'s begin your IT adventure!',
+  'Dive into your chosen IT field with tailored courses, quizzes, and resources. Track your progress, earn badges, and collaborate with peers. CoDev is your partner in mastering Information Technology.'
+];
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -19,6 +27,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     final urlOb = 'assets/images/OBJECTS.png';
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -26,126 +35,127 @@ class _MainScreenState extends State<MainScreen> {
             color: FigmaColors.sUNRISESunray,
           ),
           Positioned(
-            bottom: 0,
-            left: 0,
+            right: 0,
+            top: 0,
             child: Container(
-              width: 375,
-              height: 433,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24.0),
-                  topRight: Radius.circular(24.0),
+              width: deviceSize.width,
+              height: deviceSize.height * 0.4,
+              padding: EdgeInsets.only(
+                top: deviceSize.height * 0.1,
+              ),
+              child: Center(
+                child: Image.asset(
+                  urlOb,
                 ),
               ),
             ),
           ),
           Positioned(
-            right:44,
-            top:163.54,
-            child: Image.asset(
-                  urlOb,
-                ),
-          ),
-          Positioned(
-            top: 379,
+            top: deviceSize.height * 0.4,
             left: 0,
-              right: 0,
-              child: Center(
-                  child: Container(
-                    height: 433,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(24.0)
+            right: 0,
+            child: Container(
+              height: deviceSize.height * 0.6,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+              //margin: EdgeInsets.symmetric(vertical: 25),
+              padding: EdgeInsets.all(deviceSize.width * 0.1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  buildIndicator(),
+                  SizedBox(
+                    height: deviceSize.height * 0.025,
+                  ),
+                  Expanded(
+                    child: CarouselSlider.builder(
+                      options: CarouselOptions(
+                          enableInfiniteScroll: false,
+                          onPageChanged: (index, reason) =>
+                              setState(() => activeIndex = index),
+                          viewportFraction: 1.0),
+                      itemCount: 3,
+                      itemBuilder: (context, index, realIndex) {
+                        final title = titles[index];
+                        final subtitle = subtitles[index];
+                        return buildMainScreen(
+                            title, subtitle, index, deviceSize);
+                      },
                     ),
-                    child: Container(
-                      margin: EdgeInsets.symmetric(vertical: 25),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          buildIndicator(),
-                          SizedBox(height: 87),
-                          Center(
-                            child: CarouselSlider.builder(
-                              options: CarouselOptions(
-                                  height: 150,
-                                  enableInfiniteScroll: false,
-                                  onPageChanged: (index, reason) => setState(() => activeIndex = index)
-                              ),
-                              itemCount: 3,
-                              itemBuilder: (context, index, realIndex) {
-                                final title = titles[index];
-                                final subtitle = subtitles[index];
-                                return buildMainScreen(title, subtitle, index);
-                              },
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          Positioned(
-                            top: 0,
-                            child: Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 34, vertical: 14),
-                                child: SizedBox(
-                                  width: 250,
-                                  height: 64,
-                                  child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: FigmaColors.sUNRISEBluePrimary,
-                                          shape: ContinuousRectangleBorder(
-                                            borderRadius: BorderRadius.circular(24.0),
-                                          )),
-                                      onPressed: (){},
-                                      child: Text(
-                                        'Get Started',
-                                        style: FigmaTextStyles.mButton.copyWith(color: FigmaColors.sUNRISEWhite),
-                                      )
-                                  ),
-                                )
-                            ),
-                          ),
-                        ],
+                  ),
+                  SizedBox(height: deviceSize.height * 0.025),
+                  Container(
+                    width: deviceSize.width * 0.7,
+                    height: deviceSize.height * 0.08,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: FigmaColors.sUNRISEBluePrimary,
+                        shape: ContinuousRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Get Started',
+                        style: FigmaTextStyles.mButton
+                            .copyWith(color: FigmaColors.sUNRISEWhite),
                       ),
                     ),
-                  )
-          )
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
     );
   }
-  Widget buildMainScreen(String title, String subtitle, int index) => Column(
-      children: [
-          SizedBox(
-            width: 250,
-            child: Center(
+
+  Widget buildMainScreen(
+    String title,
+    String subtitle,
+    int index,
+    Size device,
+  ) =>
+      Column(
+        children: [
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.clip,
+            style: FigmaTextStyles.h3.copyWith(
+              color: FigmaColors.sUNRISELightCharcoal,
+            ),
+          ),
+          SizedBox(height: device.height * 0.025),
+          Expanded(
+            child: SingleChildScrollView(
               child: Text(
-                title,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.clip,
-                style: FigmaTextStyles.h3.copyWith(color: FigmaColors.sUNRISELightCharcoal),
+                subtitle,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.clip,
+                style: FigmaTextStyles.p.copyWith(
+                  color: FigmaColors.sUNRISETextGrey,
+                ),
               ),
             ),
           ),
-        SizedBox(height: 24),
-        SizedBox(
-            width: 250,
-            child: Center(
-                child: Text(
-                    subtitle,
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.clip,
-                    style: FigmaTextStyles.p.copyWith(color:FigmaColors.sUNRISETextGrey)))),
-      ],
-  );
+        ],
+      );
 
   Widget buildIndicator() => AnimatedSmoothIndicator(
-    activeIndex: activeIndex,
-    count: 3,
-    effect: ExpandingDotsEffect(
-        dotWidth: 5,
-        dotHeight: 5,
-        dotColor: Color.fromARGB(255, 171, 255, 249),
-        activeDotColor: Color(0xFF2FD1C5)
-    ),
-  );
+        activeIndex: activeIndex,
+        count: 3,
+        effect: ExpandingDotsEffect(
+          dotWidth: 5,
+          dotHeight: 5,
+          dotColor: Color.fromARGB(255, 171, 255, 249),
+          activeDotColor: Color(0xFF2FD1C5),
+        ),
+      );
 }
